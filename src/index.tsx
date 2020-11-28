85315529bcb19fb77d3c49bc69d2cae955c2e710
Ali Razzaq <59477206+AliRa22aq@users.noreply.github.com>
2020-11-28 21:20:58 +0500

    final changes

--- src/index.tsx
+++ src/index.tsx
@@ -27,14 +27,3 @@ ReactDOM.render(
 reportWebVitals();
 
 
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('/sw.js').then(function(registration) {
-      // Registration was successful
-      console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function(err) {
-      // registration failed :(
-      console.log('AAServiceWorker registration failed: ', err);
-    });
-  });
-}
--- src/svdev.ts
+++ src/svdev.ts
@@ -1,15 +1,14 @@
 export const register = () => {
 
-if ('serviceWorker' in navigator) {
-    window.addEventListener('load', function() {
-      navigator.serviceWorker.register('/sw.js').then(function(registration) {
-        // Registration was successful
-        console.log('ServiceWorker registration successful with scope: ', registration.scope);
-      }, function(err) {
-        // registration failed :(
-        console.log('ServiceWorker registration failed: ', err);
-      });
-    });
-  }
-
+    if ('serviceWorker' in navigator) {
+        window.addEventListener('load', function() {
+          navigator.serviceWorker.register('/sw.js').then(function(registration) {
+            // Registration was successful
+            console.log('ServiceWorker registration successful with scope: ', registration.scope);
+          }, function(err) {
+            // registration failed :(
+            console.log('AAServiceWorker registration failed: ', err);
+          });
+        });
+      }
 }
